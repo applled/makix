6463f5f9cf4a3f20987d7c2038dfe6128009b20f
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-12-25 20:33:16 +0530

    Create venom_setup.sh

---
+++ venom_setup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+update_packages() {
+yes|pkg upgrade
+apt update -y
+apt upgrade -y
+}
+
+
+install_pkgs() {
+clear
+echo -e "Installing required apt packages"
+yes|apt install wget python3 git neofetch curl ffmpeg mediainfo
+echo "Done"
+}
+
+
+fix_resolve() {
+clear
+echo -e "Fixing Python issue."
+wget -q https://raw.githubusercontent.com/ux-termux/venomx/main/resolver.py
+python -c """
+import os
+from sys import version_info as ver
+termux_check = os.environ.get('TERMUX_APK_RELEASE')
+if termux_check == 'F_DROID':
+    respath =f'/data/data/com.termux/files/usr/lib/python3.{ver[1]}/site-packages/dns/'
+    resname = 'resolver.py'
+    full_path = respath + resname
+    os.remove(str(full_path))
+    os.system(f'mv {resname} {respath}')
+    os.system('echo nameserver 8.8.8.8 > /data/data/com.termux/files/usr/etc/resolv.conf')
+"""
+echo "Done"
+}
+
+
+bot() {
+clear
+echo "Clonning Bot Repository."
+git clone -q https://github.com/ashwinstr/VenomX
+cd VenomX
+echo "Installing Pypi Requirments"
+pip install -r requirements.txt
+clear
+mv config.env.sample config.env
+echo -e 'Entering Config editor\nEnclose Alphanumeric variables in " " quotes.'
+sleep 5
+nano config.env
+}
+
+
+check_termux() {
+ if [[ $TERMUX_APK_RELEASE == "F_DROID" ]]; then
+    update_packages
+    install_pkgs
+    fix_resolve
+    bot
+else
+    echo -e "Play Store Termux not supported,\nDownload it from F-Droid"
+fi
+}
+
+
+check_termux
