01f646239cc4dd5302197a8ae4fe03119500eb84
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-12-26 11:21:40 +0530

    Update venom_setup.sh

--- venom_setup.sh
+++ venom_setup.sh
@@ -24,14 +24,19 @@ import os
 from sys import version_info as ver
 termux_check = os.environ.get('TERMUX_APK_RELEASE')
 if termux_check == 'F_DROID':
-    respath =f'/data/data/com.termux/files/usr/lib/python3.{ver[1]}/site-packages/dns/'
-    resname = 'resolver.py'
-    full_path = respath + resname
-    os.remove(str(full_path))
-    os.system(f'mv {resname} {respath}')
-    os.system('echo nameserver 8.8.8.8 > /data/data/com.termux/files/usr/etc/resolv.conf')
+    if ver[1] == 11:
+        respath =f'/data/data/com.termux/files/usr/lib/python3.11/site-packages/dns/'
+        resname = 'resolver.py'
+        full_path = respath + resname
+        os.remove(str(full_path))
+        os.system(f'mv {resname} {respath}')
+        os.system('echo nameserver 8.8.8.8 > /data/data/com.termux/files/usr/etc/resolv.conf')
+        if os.path.isfile(resname):
+            os.remove(resname)
+        print("Done")
+    else:
+        print("Update Python version to 3.11")
 """
-echo "Done"
 }
 
 
@@ -56,6 +61,7 @@ check_termux() {
     install_pkgs
     fix_resolve
     bot
+    echo -e "Now run ' cd VenomX ' and then ' bash run ' to start bot."
 else
     echo -e "Play Store Termux not supported,\nDownload it from F-Droid"
 fi
